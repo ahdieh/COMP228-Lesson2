baca868cfd1ac5c63d2607d8f3ff88466812cc25
ahdieh <ahdieh@yahoo.com>
2015-09-15 23:41:40 -0700

    Add private name instance variable to Program class

--- src/Program.java
+++ src/Program.java
@@ -1,8 +1,10 @@
 
 public class Program {
-
+private static String _name = "";
 	public static void main(String[] args) {
 System.out.println("Program Started...");
+_name="Ahdieh";
+System.out.println(_name);
 	}
 
 }
